090766f89f197410e27878ae801ae182eb53df4e
mkoner <amzid@hotmail.fr>
2023-09-05 18:19:42 +0000

    Implemented connect and disconnect endpoints

---
+++ controllers/AuthController.js
@@ -0,0 +1,48 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+import userUtils from '../utils/userUtils';
+
+const sha1 = require('sha1');
+
+class AuthController {
+  static async getConnect(request, response) {
+    const auth = request.headers.authorization;
+    const credentials = auth.split(' ')[1];
+    if (!credentials) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+    const [email, rowPassword] = credentials.split(':');
+    if (!rowPassword || !email) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+    const password = sha1(rowPassword);
+    const user = await dbClient.findOne({
+      email,
+      password,
+    });
+    if (!user) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    await redisClient.set(key, user._id.toString(), 24 * 3600);
+    return response.status(200).send(token);
+  }
+
+  static async getDisconnect(request, response) {
+    const { userId, key } = userUtils.getUserIdAndKey(request);
+    if (!userId) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+    await redisClient.del(key);
+    return response.status().send(204);
+  }
+
+  static getMe(request, response) {
+
+  }
+}
+
+export default AuthController;
---
+++ utils/userUtils.js
@@ -0,0 +1,14 @@
+import redisClient from './redis';
+
+const userUtils = {
+  async getUserIdAndKey(request) {
+    const obj = { userId: null, key: null };
+    const xToken = request.headers['X-Token'];
+    if (!xToken) return obj;
+    obj.key = `auth_${xToken}`;
+    obj.userId = await redisClient.get(obj.key);
+    return obj;
+  },
+};
+
+export default userUtils;
